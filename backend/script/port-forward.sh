5d9697c47be4131c9e8343dd48af3b90d96a03cf
AlexPetrusca <petruscalex@gmail.com>
2026-02-10 23:21:01 -0800

    update port-forward

--- backend/script/port-forward.sh
+++ backend/script/port-forward.sh
@@ -4,6 +4,8 @@
 
 pkill -f "kubectl port-forward svc/comethru-postgresql"
 pkill -f "kubectl port-forward svc/comethru-minio"
+pkill -f "kubectl port-forward svc/comethru-minio-console"
 
 kubectl port-forward svc/comethru-postgresql 5432:5432 &
-kubectl port-forward svc/comethru-minio 9000:9000
+kubectl port-forward svc/comethru-minio 9000:9000 &
+kubectl port-forward svc/comethru-minio-console 9001:9001
