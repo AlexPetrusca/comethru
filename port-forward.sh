1515a0048117f93872d75e8baf9c5265b6a0e3d1
AlexPetrusca <petruscalex@gmail.com>
2026-03-13 14:09:06 -0700

    use socat to forward to comethru-mocker

--- port-forward.sh
+++ port-forward.sh
@@ -8,9 +8,10 @@ pkill -f "kubectl port-forward svc/comethru-minio"
 pkill -f "kubectl port-forward svc/comethru-minio-console"
 pkill -f "kubectl port-forward svc/comethru-kafka"
 pkill -f "kubectl port-forward svc/comethru-redis-master"
-pkill -f "kubectl port-forward svc/comethru-mocker"
 pkill -f "kubectl port-forward svc/comethru-prometheus-server"
 pkill -f "kubectl port-forward svc/comethru-grafana"
+pkill -f "kubectl port-forward svc/comethru-mocker"
+pkill -f "socat"
 
 kubectl port-forward svc/comethru-backend 8081:80 &
 kubectl port-forward svc/comethru-postgresql 5432:5432 &
@@ -18,6 +19,7 @@ kubectl port-forward svc/comethru-minio 9000:9000 &
 kubectl port-forward svc/comethru-minio-console 9001:9001 &
 kubectl port-forward svc/comethru-kafka 9092:9092 &
 kubectl port-forward svc/comethru-redis-master 6379:6379 &
-kubectl port-forward svc/comethru-mocker 8099:8099 8098:8098 &
 kubectl port-forward svc/comethru-prometheus-server 9090:80 &
-kubectl port-forward svc/comethru-grafana 9091:80
+kubectl port-forward svc/comethru-grafana 9091:80 &
+kubectl port-forward svc/comethru-mocker 8099:8099 8098:8098 &
+socat TCP-LISTEN:8099,fork TCP:127.0.0.1:8099
